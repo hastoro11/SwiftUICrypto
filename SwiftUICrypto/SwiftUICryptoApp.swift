216f7f92f037cc03af1b027367622cfd089ab49c
Gabor Sornyei <info@sornyei.com>
2022-04-02 08:51:19 +0200

    Initial Commit

---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by Gabor Sornyei on 2022. 04. 02..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
